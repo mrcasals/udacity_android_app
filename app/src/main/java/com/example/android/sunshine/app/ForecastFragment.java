49a794e133c2d249eceea623f518e794041a94b1
Marc Riera <mrc2407@gmail.com>
2015-02-06 10:41:50 +0100

    Use user's preferences to get forecast data

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -2,9 +2,11 @@ package com.example.android.sunshine.app;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -94,7 +96,12 @@ public class ForecastFragment extends Fragment {
         switch(item.getItemId()) {
             case R.id.action_refresh:
                 FetchWeatherTask weatherTask = new FetchWeatherTask();
-                weatherTask.execute("94043");
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                String location = prefs.getString(
+                        getString(R.string.pref_location_key),
+                        getString(R.string.pref_location_default)
+                );
+                weatherTask.execute(location);
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
