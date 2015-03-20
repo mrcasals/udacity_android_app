241de16a901d3c83efa57df31a1f8d544b6b4c33
Marc Riera <mrc2407@gmail.com>
2015-03-20 12:33:38 +0100

    Change layout depending on item position

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -18,14 +18,39 @@ public class ForecastAdapter extends CursorAdapter {
         super(context, c, flags);
     }
 
-    /*
-        Remember that these views are reused as needed.
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+
+    /**
+     * Copy/paste note: Replace existing newView() method in ForecastAdapter with this one.
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
-
-        return view;
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+        switch (viewType) {
+            case VIEW_TYPE_TODAY: {
+                layoutId = R.layout.list_item_forecast_today;
+                break;
+            }
+            case VIEW_TYPE_FUTURE_DAY: {
+                layoutId = R.layout.list_item_forecast;
+                break;
+            }
+        }
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*
